1afbdad02c00e0473504c0064e8d87d9ca65d365
ardiandp <ardiandp@gmail.com>
2020-03-16 21:02:38 -0700

    nambah fiel barang

---
+++ application/controllers/Barang.php
@@ -0,0 +1,15 @@
+<?php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Barang extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Barang.php */
+/* Location: ./application/controllers/Barang.php */
