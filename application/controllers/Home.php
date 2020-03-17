c3e133605309a7831e812937df8be1eabffc18b5
ardiandp <ardiandp@gmail.com>
2020-03-16 21:00:54 -0700

    saya nambah file home dan edit welocome

---
+++ application/controllers/Home.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	
+	class Home extends CI_Controller {
+	
+		public function index()
+		{
+			
+		}
+	
+	}
+	
+	/* End of file Home.php */
+	/* Location: ./application/controllers/Home.php */
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Home.php */
+/* Location: ./application/controllers/Home.php */
